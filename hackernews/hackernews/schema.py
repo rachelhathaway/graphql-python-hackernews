05e159e57c441f616031ba42042bb6ee322773ae
rachelhathaway <rachel.e.hathaway@gmail.com>
2018-03-01 15:21:16 -0600

    add link mutation

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -5,4 +5,7 @@ import links.schema
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
