b427ac6e902554249659bfb5d3e8f83f48006516
rachelhathaway <rachel.e.hathaway@gmail.com>
2018-03-05 15:33:40 -0600

    add pagination

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -17,14 +17,28 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+
         if search:
             filter = Q(url__icontains=search) | Q(description__icontains=search)
-            return Link.objects.filter(filter)
-        return Link.objects.all()
+            qs = qs.filter(filter)
+
+        if skip:
+            qs = qs[skip::]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
